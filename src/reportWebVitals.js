743480953b3774453f732b0927f3a0253eca4b19
Max Moeschinger <max@momentech.no>
2021-03-08 20:46:41 +0100

    add path to web vitals

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,8 @@
 import { getLCP, getFID, getCLS, getFCP, getTTFB } from 'web-vitals';
 import request from './helpers/request';
 
+const path = String(window.location.pathname);
+
 const report = async (name, value) => {
     await request({
         url: '/tracking/web-vitals',
@@ -8,6 +10,7 @@ const report = async (name, value) => {
         data: {
             name,
             value,
+            path,
         },
     });
 };
